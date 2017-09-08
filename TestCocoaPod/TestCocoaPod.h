dd0c802ae94aad020ebaf6c6f074c3c3bbf29d83
Brian H. Mayo <brian.h.mayo@gmail.com>
2017-09-07 20:56:46 -0400

    Initial Commit

---
+++ TestCocoaPod/TestCocoaPod.h
@@ -0,0 +1,19 @@
+//
+//  TestCocoaPod.h
+//  TestCocoaPod
+//
+//  Created by Brian H. Mayo on 9/7/17.
+//  Copyright © 2017 Vanguard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCocoaPod.
+FOUNDATION_EXPORT double TestCocoaPodVersionNumber;
+
+//! Project version string for TestCocoaPod.
+FOUNDATION_EXPORT const unsigned char TestCocoaPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCocoaPod/PublicHeader.h>
+
+
